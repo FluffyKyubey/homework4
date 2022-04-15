43b40d30e008c5019dc6852c7ef4909a66ba5584
John Hull <john.alex.hull@gmail.com>
2022-04-14 19:56:27 -0700

    started homework made all my files

---
+++ Assets/js/questions.js
@@ -0,0 +1,35 @@
+var questions = [
+    {
+      title: "What is the total of '100 + 32'?",
+      choices: ["132", "145", "68", "72"],
+      answer: "132"
+    },
+    {
+      title: "What is the result of '9 * 9' ?",
+      choices: ["18", "1", "81", "82"],
+      answer: "81"
+    },
+    {
+      title: "What is the result of ' 144/12 '.",
+      choices: [
+        "132",
+        "12",
+        "156",
+        "1"
+      ],
+      answer: "12"
+    },
+    {
+      title:
+        "Which of the following are prime numbers",
+      choices: ["2", "13", "97", "all of the above"],
+      answer: "all of the above"
+    },
+    {
+      title:
+        "I am an odd number. Take away one letter and I become even. What number am I? ",
+      choices: ["Seven", "Nine", "Four", "Thirteen"],
+      answer: "Seven"
+    }
+  ];
+  
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # homework4
+
+this webpage is a quick quiz that the user can take and it will return a score 
+
+the user is offered multiple choice questions and when they get an answer wrong the total time is reduced
+
+the score is based off of how much time is remaining when they finish all the questions 
